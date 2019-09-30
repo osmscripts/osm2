1dd63b53435736d34f073ddecfbebdbf66f9e4e9
Vladislav Osmianskij <vlaosm@gmail.com>
2019-09-30 23:13:39 +0300

    `osm in` and `osm translate` commands

---
+++ src/Commands/Translate.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace OsmScripts\Osm\Commands;
+
+use OsmScripts\Osm\ModuleCommand;
+use Symfony\Component\Console\Input\InputArgument;
+
+/** @noinspection PhpUnused */
+
+/**
+ * `translate` shell command class.
+ *
+ * @property string $string
+ * @property string $quoted_string
+ */
+class Translate extends ModuleCommand
+{
+    #region Properties
+    public function __get($property) {
+        switch ($property) {
+            case 'string': return $this->string = $this->input->getArgument('string');
+            case 'quoted_string': return $this->quoted_string = '"' .  addslashes($this->string) . '"';
+        }
+
+        return parent::__get($property);
+    }
+    #endregion
+
+    protected function configure() {
+        parent::configure();
+        $this->setDescription("Adds string to translation file")
+            ->addArgument('string', InputArgument::REQUIRED,
+                "String to be translated");
+    }
+
+    protected function handle() {
+        $this->shell->cd($this->module_->path, function() {
+            $this->registerString();
+        });
+    }
+
+    protected function registerString() {
+        $filename = "config/translations/en_US.php";
+        $contents = is_file($filename)
+            ? file_get_contents($filename)
+            : $this->files->render('config_file');
+
+        if (mb_strrpos($contents, $this->quoted_string) !== false) {
+            return;
+        }
+
+        $this->files->save($filename, $this->php->edit($contents, function() {
+            $this->php->insertBefore($this->php->last('];'),
+                "    {$this->quoted_string} => {$this->quoted_string},\n");
+        }));
+    }
+}
